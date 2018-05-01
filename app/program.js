64f5778d9ee8fd2146bb77cb03cbf3755b2a7895
Joanna Buchała <joanna.b2@op.pl>
2018-05-01 20:51:57 +0200

    add modules

--- app/program.js
+++ app/program.js
@@ -1,6 +1,4 @@
-var OSinfo = require('../modules/OSinfo');
-
-OSinfo.print();
+var os = require('os');
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
@@ -23,3 +21,24 @@ process.stdin.on('readable', function() {
         };
     }
 });
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
+getOSinfo();
